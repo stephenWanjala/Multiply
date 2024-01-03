7bbcc7f51188b4dc108a2fdfb0b42203022ae5fa
wanjala stephen <stephenwanjala145@gmail.com>
2024-01-04 01:45:27 +0300

    enableEdgeToEdge

--- app/src/main/java/com/github/stephenwanjala/multiply/MainActivity.kt
+++ app/src/main/java/com/github/stephenwanjala/multiply/MainActivity.kt
@@ -2,11 +2,15 @@ package com.github.stephenwanjala.multiply
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.github.stephenwanjala.multiply.presentation.NavGraphs
 import com.github.stephenwanjala.multiply.ui.theme.MultiplyTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -16,6 +20,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.Transparent.toArgb(),
+                Color.Transparent.toArgb(),
+
+                )
+        )
         setContent {
             MultiplyTheme {
                 // A surface container using the 'background' color from the theme
