154dc028ac140d50b2fd79267fdfc29f73d0ad6a
Wanjala Stephen <stephenwanjala145@gmail.com>
2025-11-20 00:46:01 +0300

    build: Upgrade dependencies and refine Kotlin compiler options
    - Bump `lifecycleRuntimeKtx` to `2.10.0` and `activityCompose` to `1.12.0`.
    - Update `composeBom` to `2025.11.01` and `material3` to `1.5.0-alpha09`.
    - Upgrade `datastorePreferences` to `1.2.0`.
    - Migrate `kotlinOptions` to the newer `kotlin.compilerOptions` block in `app/build.gradle.kts` using `JvmTarget`.
    - Add `-Xannotation-default-target=param-property` to free compiler args.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -35,8 +37,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("11")
+            freeCompilerArgs.add("-Xannotation-default-target=param-property")
+        }
     }
     buildFeatures {
         compose = true
